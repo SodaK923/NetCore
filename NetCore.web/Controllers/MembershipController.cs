f127b8319d7f9ea289cd3f158baad3f9c42a5826
Soda <k.sua923@gmail.com>
2026-02-09 17:42:59 +0900

    초기 커밋.

---
+++ NetCore.web/Controllers/MembershipController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore.web.Controllers
+{
+    public class MembershipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
