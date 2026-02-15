8218ce010a7e98f004b6949b873ef3d55b0e2854
Soda <k.sua923@gmail.com>
2026-02-16 01:46:10 +0900

    OnModelCreating 형식 수정

--- NetCore.Services/Data/CodeFirstDbContext.cs
+++ NetCore.Services/Data/CodeFirstDbContext.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 namespace NetCore.Services.Data
 {
     // 2. Fulent API 방식 - Code First
+    // Entity → (무엇을 설정할 건지 선택) → (그것의 옵션 설정)
 
 
     // 상속
@@ -24,6 +25,9 @@ namespace NetCore.Services.Data
 
         // DB 테이블 리스트 지정
         public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<UserRolesByUser> UserRoleByUsers { get; set; }
+
         // 메서드 상속: 부모 클래스의 메서드를 재정의하여 추가 기능 구현
         // 부모 클래스에서 OnModelCreating 메서드가 가상 메서드로 정의되어 있어 재정의 가능
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -33,18 +37,31 @@ namespace NetCore.Services.Data
             // 4가지 작업
             // DB 테이블이름 변경
             modelBuilder.Entity<User>().ToTable(name: "User");
+            modelBuilder.Entity<UserRole>().ToTable(name: "UserRole");
+            modelBuilder.Entity<UserRolesByUser>().ToTable(name: "UserRolesByUser");
 
             // 복합키 지정(두 개 이상의 컬럼을 기본키로 설정)
             modelBuilder.Entity<UserRolesByUser>().HasKey(c => new { c.UserId, c.RoleId });
 
             // 컬럼 기본값 지정
-            modelBuilder.Entity<User>(e =>
-            {
-                e.Property(c => c.IsMembershipWithdrawn).HasDefaultValue(value: false);
-            });
+            //modelBuilder.Entity<User>(e =>
+            //{
+            //    e.Property(c => c.IsMembershipWithdrawn).HasDefaultValue(value: false);
+            //});
+            modelBuilder.Entity<User>().Property(c => c.IsMembershipWithdrawn).HasDefaultValue(value: false);
+               
 
             // 인덱스 지정: 중복이 되지 않도록
-            modelBuilder.Entity<User>().HasIndex(c => new { c.UserEmail }).IsUnique(unique: true);
+            modelBuilder.Entity<User>().HasIndex(c => c.UserEmail ).IsUnique(unique: true);
+
+
+            // User거 이렇게 다 합쳐도 됨 (e는 User 엔티티를 나타내는 매개변수)
+            //modelBuilder.Entity<User>(e =>
+            //{
+            //    e.ToTable(name: "User");
+            //    e.HasIndex(c => new { c.UserEmail }).IsUnique(unique: true);
+            //    e.Property(c => c.IsMembershipWithdrawn).HasDefaultValue(value: false);
+            //});
         }
     }
 }
