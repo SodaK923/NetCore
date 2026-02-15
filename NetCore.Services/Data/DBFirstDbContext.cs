20fa22a2af1ea826d3e38d7eb7ac654fdcd0d283
Soda <k.sua923@gmail.com>
2026-02-16 02:13:50 +0900

    DBFirstContext 생성, Data 프로젝트에 Classes 생성하고 User/UserRole/UserRolesByUser 클래스 추가

--- NetCore.Services/Data/DBFirstDbContext.cs
+++ NetCore.Services/Data/DBFirstDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace NetCore.Services.Data
 {
-    internal class DBFirstDbContext
+    public class DBFirstDbContext : DbContext
     {
+        public DBFirstDbContext(DbContextOptions<DBFirstDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // DB First 방식에서는 데이터베이스에서 테이블과 컬럼 정보를 가져와서 모델을 자동으로 생성하기 때문에
+            // 별도의 모델 설정이 필요하지 않습니다.
+        }
     }
 }
